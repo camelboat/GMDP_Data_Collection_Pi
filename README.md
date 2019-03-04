e00ad8a4d43bacfbe77bb308d7faa7c1aa89d8c0
camelboat <camelboat@sjtu.edu.cn>
2019-03-04 08:00:26 +0000

    修改：     README.md 	修改：     temperature_PIR.py

--- README.md
+++ README.md
@@ -5,19 +5,27 @@ This program is running on raspberry pi, with an Arduino Uno connected to it thr
 
 Current setting of data reading interval is 100ms, which can be modified in the Arduino program, but cannot be changed by the instruction from pi in real time.
 
+Now four PIR sensors are added in one room, we want to estimate the number of people in the room by counting the total trigger times from all PIR sensors.
+
 ## Usage
 Typical data transferred from Arduino Uno are:
 
 ```python
-"t21.23\n" # for temperature
-"p0\n"     # for PIR sensor
+"t21.23\r\n" # for temperature
+"10\r\n"     # for PIR sensor 1
+"20\r\n"     # for PIR sensor 2
+"30\r\n"     # for PIR sensor 3
+"40\r\n"     # for PIR sensor 4
 ```
 
 After decoding, these values would be stored in variables 
 
 ```python
 float temperature # 21.23 (in Celcius degree)
-int any_people    # 0 (0 for not detecting people, 1 for detecting people)
+int any_people[4] # [0,0,0,0] (0 for not detecting people, 1 for detecting people)
+int trigger       # 25 (total trigger numbers within the interval) 
 ```
 ## Future Work
 Since the function of counting people by camera using cv method currently doesn't work well enough, we are thinking about estimating the number of people by the combination of camera videos and PIR data. To do this, we may be going to add four or more PIR sensors in one room, and estimating the amount of people by counting the number of triggers. We are wondering if this computing process should be done on ThingSpeak or be done locally.
+
+Currently, we only use delay time set from the Arduino program to count the times that we need to read from the PIR sensors within the interval, but it's not accurate since the reading process also takes time. Maybe the timing function should be totally moved to the program on Pi.
--- temperature_PIR.py
+++ temperature_PIR.py
@@ -1,35 +1,56 @@
 import serial
-#import RPi.GPIO as GPIO
 import time
-#import re
-import cv2
 
+#import cv2
+#import RPi.GPIO as GPIO
+#import re
 #import sys
 #mport os
 
-ser=serial.Serial("/dev/ttyACM0",9600)
-ser.baudrate=9600
+ser = serial.Serial("/dev/ttyACM0",9600)
+ser.baudrate = 9600
 ser.timeout = 2
-
 status = True
+any_people = [0,0,0,0]
+
+delay = 100 # ms
+interval = 5 # s
+read_num = interval * 1000 / delay * 4 # This is not very accurate 
+count = 0
+trigger = 0
+
+# Discard the first ten line data
+# to avoid format problem
+for i in range(1, 10):
+    read_ser = ser.readline()
 
 while True:
     try:
-        read_ser=ser.readline()
-        data_ser=read_ser.decode('utf-8')
-        
-        # temperature
-        if data_ser[0]=="t":
-            temperature = float(data_ser[1:])
-            print("Temperature is ", temperature, "Celcius degree")
+        read_ser = ser.readline()
+        #print(read_ser)
 
-        # any_people, 0 for not detecting people, 1 for detecting people
-        elif data_ser[0]=="p":
-            any_people = int(data_ser[1:])
-            if any_people == 0:
-                print("There is no one in the room")
-            else:
-                print("There are someone in the room")
+        data_ser = read_ser.decode('utf-8')
+        flag = data_ser[0]
+        #print(flag)
+
+        # temperature
+        if flag == "t":
+            temperature = float(data_ser[1:])
+            #print("Temperature is %f Celcius degree" % temperature)
+            #print(temperature)
+
+        # PIR sensors
+        else:
+            any_people[int(flag)-1] = int(data_ser[1])
+            #print(any_people[int(flag)-1])
+
+            count+=1
+            if any_people[int(flag)-1] == 1:
+                trigger+=1
+            if count == read_num:
+                print("In last %d s, PIR sensors have been triggered for %d times" % (interval, trigger))
+                count = 0
+                trigger = 0
 
     except KeyboardInterrupt:
         break
