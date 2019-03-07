016541915b07b74c7d9d8a4c1c040079023c8cf9
camelboat <camelboat@sjtu.edu.cn>
2019-03-07 13:50:20 +0000

    修改：     temperature_PIR.py

--- temperature_PIR.py
+++ temperature_PIR.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import csv
 
 #import cv2
 #import RPi.GPIO as GPIO
@@ -19,6 +20,9 @@ read_num = interval * 1000 / delay * 4 # This is not very accurate
 count = 0
 trigger = 0
 
+data_list = [['Temperature'], ['PIR_1'], ['PIR_2'], ['PIR_3'], ['PIR_4']]
+
+
 # Discard the first ten line data
 # to avoid format problem
 for i in range(1, 10):
@@ -31,18 +35,23 @@ while True:
 
         data_ser = read_ser.decode('utf-8')
         flag = data_ser[0]
+
         #print(flag)
 
         # temperature
-        if flag == "t":
+        if flag == "t"::ewq
+
             temperature = float(data_ser[1:])
             #print("Temperature is %f Celcius degree" % temperature)
-            #print(temperature)
+            print(temperature)
+            data_list[0].append(temperature)
+            
 
         # PIR sensors
         else:
             any_people[int(flag)-1] = int(data_ser[1])
-            #print(any_people[int(flag)-1])
+            print(any_people[int(flag)-1])
+            data_list[int(flag)-1].append(any_people[int(flag-1)])
 
             count+=1
             if any_people[int(flag)-1] == 1:
@@ -53,4 +62,8 @@ while True:
                 trigger = 0
 
     except KeyboardInterrupt:
+        with open('Temperature_PIR.csv', mode='w') as output_file:
+            output_fd = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            output.writerows(data_list)
+        csvFile.close()
         break
