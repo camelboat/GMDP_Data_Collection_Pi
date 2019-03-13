ce683262d683715d4900c2e2ccab5b8920d7c792
camelboat <camelboat@sjtu.edu.cn>
2019-03-13 09:37:18 +0000

    删除：     data.csv 	修改：     temperature_PIR.py

--- temperature_PIR.py
+++ temperature_PIR.py
@@ -45,6 +45,7 @@ while True:
     try:
         read_ser = ser.readline()
         #print(read_ser)
+
         data_ser = read_ser.decode('ISO-8859-1')
         #data_ser = read_ser.decode('utf-8')
         flag = data_ser[0]
