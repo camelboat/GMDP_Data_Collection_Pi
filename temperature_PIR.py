6d02197c9bcf35c97dec1c75c5c0e6497be69a93
camelboat <camelboat@sjtu.edu.cn>
2019-04-10 06:45:56 +0800

    data list modification

--- temperature_PIR.py
+++ temperature_PIR.py
@@ -32,8 +32,7 @@ count = 0
 
 baseURL_temperature = 'https://api.thingspeak.com/update?api_key=T9PJ3W9K7NSQ6AT8&field1=0'
 baseURL_PIR = 'https://api.thingspeak.com/update?api_key=T9PJ3W9K7NSQ6AT8&field2=0'
-upload_last_temperature = time.time()
-upload_last_PIR = time.time()
+upload_last = time.time()
 data_list_temperature = []
 data_list_PIR = []
 data_list_PIR_1 = []
@@ -69,26 +68,25 @@ while True:
             temperature = float(data_ser[1:])
             print(temperature)
             data_list_temperature.append(temperature)
-            # update data
-            if time.time() - upload_last_temperature > 15:
-                upload_data(baseURL_temperature, statistics.mean(data_list_temperature))
-                upload_last_temperature = time.time()
-                data_list_temperature.clear()
  
         # PIR sensors
         else:
             any_people[int(flag)-1] = int(data_ser[1])
             print(any_people[int(flag)-1])
             data_list_PIR[int(flag)-1].append(any_people[int(flag)-1])
-            if time.time() - upload_last_PIR > 15:
-                trigger_sum = 0
-                for i in range(0, 3):
-                    trigger_sum += sum(data_list_PIR[i])
-                print("hahahahahahahahahahahahahahahaha")
-                upload_data(baseURL_PIR, trigger_sum)
-                upload_last_PIR = time.time()
-                for i in range(0, 3):
-                    data_list_PIR[i].clear()
+
+        # update data
+        if (time.time() - upload_last > 15) and (time.time() - upload_last < 30):
+            upload_data(baseURL_temperature, statistics.mean(data_list_temperature))
+            data_list_temperature.clear()
+        elif time.time() - upload_last > 30:
+            trigger_sum = 0
+            for i in range(0, 3):
+                trigger_sum += sum(data_list_PIR[i])
+            upload_data(baseURL_PIR, trigger_sum)
+            upload_last_PIR = time.time()
+            for i in range(0, 3):
+                data_list_PIR[i].clear()
 
 
     except KeyboardInterrupt:
