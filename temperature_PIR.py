a0e74b7f8dc1a342fd3cf72fc953c646f8237cec
camelboat <camelboat@sjtu.edu.cn>
2019-04-10 10:27:46 +0800

    data upload logic modification

--- temperature_PIR.py
+++ temperature_PIR.py
@@ -86,6 +86,7 @@ while True:
             trigger_sum = 0
             for i in range(0, 3):
                 trigger_sum += sum(data_list_PIR[i])
+                print('add' + str(data_list_PIR[i]))
                 data_list_PIR[i].clear
             upload_data(baseURL_PIR, trigger_sum)
             upload_PIR_status = 1
